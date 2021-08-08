8c1e9989b9b6fbfa32316804ed6c1ea2140e6086
Deepanshu <deepupant2002@gmail.com>
2021-08-08 13:43:31 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,18 @@
+# A = CRIM 
+data = {'CRIM' : 0.2,
+                    'ZN' : 5,
+                    'INDUS' : 3.1,
+                    'CHAS' : 0,
+                    'NOX' : 0.325,
+                    'RM' : 5.146,
+                    'AGE' : 70.2,
+                    'DIS'  : 3.5,
+                    'RAD' : 2,
+                    'TAX' : 260,
+                    'PTRATIO' : 19,
+                    'B' : 400,
+                    'LSTAT' : 9}
+
+
+
+
